38bf3507b498988bdd4764e94e6f41ec9b987c01
ZacharyGrubb <zacharygrubb@gmail.com>
2022-03-27 11:39:49 -0400

    Added script.js file with variables for buildings, large objects for purchasables, and a function for purchasing a building

---
+++ script.js
@@ -0,0 +1,49 @@
+let onionClicksStart = 1;
+let onionClickPower = 10000000;
+let onionFinalCalc = 0;
+let onionFinalCalcDisplay = 0;
+let onionFarmerPower = 0.1;
+let onionFieldPower = 0.8;
+let onionTruckPower = 7.5;
+let onionStorePower = 53.0;
+let onionMlmPower = 600.0;
+let onionPrinterPower = 5200.0;
+let onionRepublicPower = 36000.0;
+
+
+const upgrades = {
+    names : ['Reinforced Mouse', 'Double Click', 'Grippy Gloves', 'Safer Pestisides', 'Pit Stops', 'Farmers Markets', 'Seasonal Contracts'],
+    costs : [100, 500, 1000, 60000, 700000, 8000000, 80000],
+    start : [false, false, false, false, false, false, false],
+    visible : [false, false, false, false, false, false, false]
+};
+
+const buildings = {
+    names : ['Onion Farmer', 'Onion Field', 'Onion Truck', 'Onion Store', 'Onion MLM', 'Onion Printer', 'Onion Republic'],
+    costs : [100, 1500, 17000, 200000, 3000000, 40000000, 500000000],
+    start : [0, 0, 0, 0, 0, 0, 0],
+    set : [false, false, false, false, false, false, false],
+    price : [onionFarmerPrice, onionFieldPrice, onionTruckPrice, onionStorePrice, onionMlmPrice, onionPrinterPrice, onionRepublicPrice],
+    count : [0, 0, 0, 0, 0, 0, 0]
+};
+
+function buyBuilding(index) {
+    if ((buildings.costs[index] <= onionFinalCalc) && (buildings.set[index] == false)){
+        onionFinalCalc -= buildings.costs[index];
+        buildings.costs[index] = buildings.costs[index] * 1.15;
+        onionTotal.textContent = `${onionFinalCalc} onions clicked`;
+        buildings.start[index] = buildings.start[index] + 1;
+        buildings.count[index] = buildings.count[index] + 1;
+        buyBuildingSet(index);
+        statsDisplay();
+        buildings.set[index] = true;
+    } else if ((buildings.costs[index] <= onionFinalCalc) && (buildings.set)) {
+        onionFinalCalc -= buildings.costs[index];
+        buildings.costs[index] = buildings.costs[index] * 1.15;
+        buildings.start[index] = buildings.start[index] + 1;
+        buildings.count[index] = buildings.count[index] + 1;
+        statsDisplay();
+    } else {
+        alert('get your money up');
+    }
+}
