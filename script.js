2ae883c7bc98e52588dbed649c0d0256dfd0ba3e
ZacharyGrubb <zacharygrubb@gmail.com>
2022-03-30 19:07:02 -0400

    Added all of the DOM selectors I'll need to the script, as well as the building interval and activation functions.

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+const onion = document.querySelector('#onion');
+const onionTotal = document.querySelector('#onion-total');
+const clickCounter = document.querySelector('#click-counter');
+const reinforcedMouse = document.querySelector('#upgrades-reinforcedmouse');
+const doubleClick = document.querySelector('#upgrades-doubleclick');
+const grippyGloves = document.querySelector('#upgrades-grippygloves');
+const saferPestisides = document.querySelector('#upgrades-saferpestisides');
+const pitStops = document.querySelector('#upgrades-pitstops');
+const farmersMarkets = document.querySelector('#upgrades-farmersmarkets');
+const seasonalContracts = document.querySelector('#upgrades-seasonalcontracts');
+const onionFarmer = document.querySelector('#buildings-onion-farmer');
+const onionFarmerPrice = document.querySelector('#onion-farmer-price');
+const onionFarmerCount = document.querySelector('#onion-farmer-count');
+const buildingOpsFarmer = document.querySelector('#building-ops-farmer');
+const onionField = document.querySelector('#buildings-onion-field');
+const onionFieldPrice = document.querySelector('#onion-field-price');
+const onionFieldCount = document.querySelector('#onion-field-count');
+const buildingOpsField = document.querySelector('#building-ops-field');
+const onionTruck = document.querySelector('#buildings-onion-truck');
+const onionTruckPrice = document.querySelector('#onion-truck-price');
+const onionTruckCount = document.querySelector('#onion-truck-count');
+const buildingOpsTruck = document.querySelector('#building-ops-truck');
+const onionStore = document.querySelector('#buildings-onion-store');
+const onionStorePrice = document.querySelector('#onion-store-price');
+const onionStoreCount = document.querySelector('#onion-store-count');
+const buildingOpsStore = document.querySelector('#building-ops-store');
+const onionMlm = document.querySelector('#buildings-onion-mlm');
+const onionMlmPrice = document.querySelector('#onion-mlm-price');
+const onionMlmCount = document.querySelector('#onion-mlm-count');
+const buildingOpsMlm = document.querySelector('#building-ops-mlm');
+const onionPrinter = document.querySelector('#buildings-onion-printer');
+const onionPrinterPrice = document.querySelector('#onion-printer-price');
+const onionPrinterCount = document.querySelector('#onion-printer-count');
+const buildingOpsPrinter = document.querySelector('#building-ops-printer');
+const onionRepublic = document.querySelector('#buildings-onion-republic');
+const onionRepublicPrice = document.querySelector('#onion-republic-price');
+const onionRepublicCount = document.querySelector('#onion-republic-count');
+const buildingOpsRepublic = document.querySelector('#building-ops-republic');
+
 let onionClicksStart = 1;
 let onionClickPower = 10000000;
 let onionFinalCalc = 0;
@@ -46,6 +85,52 @@ function buyBuilding(index) {
     } else {
         alert('get your money up');
     }
+}
+
+function buyBuildingSet(index) {
+    if (index == 0) {
+        setInterval(onionFarmerEffect, 100);
+    }
+    if (index == 1) {
+        setInterval(onionFieldEffect, 100);
+    }
+    if (index == 2) {
+        setInterval(onionTruckEffect, 100);
+    }
+    if (index == 3) {
+        setInterval(onionStoreEffect, 100);
+    }
+    if (index == 4) {
+        setInterval(onionMlmEffect, 100);
+    }
+    if (index == 5) {
+        setInterval(onionPrinterEffect, 100);
+    }
+    if (index == 6) {
+        setInterval(onionRepublicEffect, 100);
+    }
+}
+
+function onionFarmerEffect() {
+    onionFinalCalc = onionFinalCalc + onionFarmerPower * buildings.start[0];
+}
+function onionFieldEffect() {
+    onionFinalCalc = onionFinalCalc + onionFieldPower * buildings.start[1];
+}
+function onionTruckEffect() {
+    onionFinalCalc = onionFinalCalc + onionTruckPower * buildings.start[2];
+}
+function onionStoreEffect() {
+    onionFinalCalc = onionFinalCalc + onionStorePower * buildings.start[3];
+}
+function onionMlmEffect() {
+    onionFinalCalc = onionFinalCalc + onionMlmPower * buildings.start[4];
+}
+function onionPrinterEffect() {
+    onionFinalCalc = onionFinalCalc + onionPrinterPower * buildings.start[5];
+}
+function onionRepublicEffect() {
+    onionFinalCalc = onionFinalCalc + onionRepublicPower * buildings.start[6];
 }
 
 function onionClickCounter() {
